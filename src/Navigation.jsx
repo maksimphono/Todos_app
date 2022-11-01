b7bde33355e2d9de2ae7315df068862ffa5db1c4
Maxun <trukhinmaksimof@mail.ru>
2022-11-01 16:09:38 +0800

    starting do routers

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -8,12 +8,13 @@ function Navigation() {
   return (
     <Navbar expand="lg">
       <Container>
-        <Navbar.Brand href="#home"><img src = {todo_logo}/> Todo App</Navbar.Brand>
+        <Navbar.Brand href="/"><img src = {todo_logo}/> Todo App</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="/">My tasks</Nav.Link>
+            <Nav.Link href="/about">About us</Nav.Link>
+            <Nav.Link href="/contact">Contact us</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
