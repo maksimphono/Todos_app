46dc9f511d65bbdddd2e88c4d1ab31bd04cd0f06
Maxun <trukhinmaksimof@mail.ru>
2022-11-03 15:49:40 +0800

    trying to fix local wtorage problems

--- src/pages/home_page/scenes/TodosScene.jsx
+++ src/pages/home_page/scenes/TodosScene.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Accordion from "react-bootstrap/Accordion"
 import {Button, Container} from "react-bootstrap"
 import TodoScene from "./todo"
 import "../../../css/style.css"
 
 export default function TodosScene(props){
+
+    useEffect(() => {
+        console.log("Render component 'TodosScene', it's props : ")
+        console.table(props);
+        console.log("Todos : ");
+        console.log(props.todos);
+    }, [props.todos]);
+
     return (
         <Container>
             <h1 className="display-4 d-flex m-auto mt-3">{props.todos.length && "You currently have these task to do:" || "You don't have tasks now, relax"}</h1>
