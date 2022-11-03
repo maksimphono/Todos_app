88df7ab3f9d8916806434799a80952dfd7f22f89
Maxun <trukhinmaksimof@mail.ru>
2022-11-03 19:57:51 +0800

    added '+' button at the top of TodosScene

--- src/pages/home_page/scenes/TodosScene.jsx
+++ src/pages/home_page/scenes/TodosScene.jsx
@@ -15,7 +15,16 @@ export default function TodosScene(props){
 
     return (
         <Container>
-            <h1 className="display-4 d-flex m-auto mt-3">{props.todos.length && "You currently have these task to do:" || "You don't have tasks now, relax"}</h1>
+            <div className="d-flex flex-row justify-content-between align-items-end">
+                <h1 className="display-4 d-flex m-auto mt-3">
+                    {props.todos.length && "You currently have these task to do:" || "You don't have tasks now, relax"}
+                </h1>
+                <Button id="new_task_tiny" variant="light" onClick={props.handleNewTask}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
+                    </svg>
+                </Button>
+            </div>
             <Accordion className="todos-accordion mt-3" defaultActiveKey="0">
                 {console.log("Ready to render 'Todo' with props :", props.todos[0])}
                 { props.todos.map((todo, i) => (
