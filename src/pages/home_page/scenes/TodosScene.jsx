166559bcf22352e3ceb9f9798113f42eb4e460cd
Maxun <trukhinmaksimof@mail.ru>
2022-11-03 16:06:03 +0800

    trying to fix local wtorage problems

--- src/pages/home_page/scenes/TodosScene.jsx
+++ src/pages/home_page/scenes/TodosScene.jsx
@@ -17,23 +17,16 @@ export default function TodosScene(props){
         <Container>
             <h1 className="display-4 d-flex m-auto mt-3">{props.todos.length && "You currently have these task to do:" || "You don't have tasks now, relax"}</h1>
             <Accordion className="todos-accordion mt-3" defaultActiveKey="0">
-                {(props.todos != null) && props.todos.map((todo, i) => {
-                    if (todo != null) {
-                        console.log("Ready to render todo ", todo.title, todo.description);
-                        <TodoScene
-                        key = {i}
-                        _key = {i}
-                        title = {todo.title}
-                        date = {todo.date} 
-                        description = {todo.description}
-                        handleCompleteTask = {props.handleCompleteTask}
-                        handleEditTask = {props.handleEditTask}
-                        handleDeleteTask = {props.handleDeleteTask} 
-                        />
-                    }
-                    
-                    }
-                )}
+                <TodoScene
+                    key = {0}
+                    _key = {0}
+                    title = {todos[0].title}
+                    date = {todos[0].date} 
+                    description = {todos[0].description}
+                    handleCompleteTask = {props.handleCompleteTask}
+                    handleEditTask = {props.handleEditTask}
+                    handleDeleteTask = {props.handleDeleteTask} 
+                />
             </Accordion>
             <Button id = "new_task" variant = "light" onClick={props.handleNewTask}>+ New</Button>
         </Container>
