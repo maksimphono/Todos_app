7b79d656097de700cbd34f8d50912b84806382d3
Maxun <trukhinmaksimof@mail.ru>
2022-11-03 16:20:29 +0800

    try to deploy 10 (hope final)

--- src/pages/home_page/scenes/TodosScene.jsx
+++ src/pages/home_page/scenes/TodosScene.jsx
@@ -18,16 +18,22 @@ export default function TodosScene(props){
             <h1 className="display-4 d-flex m-auto mt-3">{props.todos.length && "You currently have these task to do:" || "You don't have tasks now, relax"}</h1>
             <Accordion className="todos-accordion mt-3" defaultActiveKey="0">
                 {console.log("Ready to render 'Todo' with props :", props.todos[0])}
-                <TodoScene
-                    key = {0}
-                    _key = {0}
-                    title = {props.todos[0].title}
-                    date = {props.todos[0].date} 
-                    description = {props.todos[0].description}
-                    handleCompleteTask = {props.handleCompleteTask}
-                    handleEditTask = {props.handleEditTask}
-                    handleDeleteTask = {props.handleDeleteTask} 
-                />
+                {(props.todos != null) && props.todos.map((todo, i) => {
+                    if (todo != null) {
+                        console.log("Ready to render todo ", todo.title, todo.description);
+                        <TodoScene
+                        key = {i}
+                        _key = {i}
+                        title = {todo.title}
+                        date = {todo.date} 
+                        description = {todo.description}
+                        handleCompleteTask = {props.handleCompleteTask}
+                        handleEditTask = {props.handleEditTask}
+                        handleDeleteTask = {props.handleDeleteTask} 
+                        />
+                    }
+                    }
+                )}
             </Accordion>
             <Button id = "new_task" variant = "light" onClick={props.handleNewTask}>+ New</Button>
         </Container>
